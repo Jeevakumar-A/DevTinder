540d4e4b7b708889e2534da8241f4cdf894bb6d1
Jeevakumar-A <jeevajeeva40348@gmail.com>
2025-09-22 16:36:39 +0530

    Create a Express server

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 const express =require("express");//import express from "express";
 const app=express();//create an express application
 
-app.use("/test",(req,res)=>{
-    res.send("Hello form node.js app")
+
+app.get("/test",(req,res)=>{
+    res.send("Hello form get the data")
 })
-app.use("/test2",(req,res)=>{
-    res.send("Hello form node.js server test 2")
-})
-app.use("/test3",(req,res)=>{
-    res.send("Hello form node.js server test 3")
-})
-app.use("/test4",(req,res)=>{
-    res.send("Hello form node.js server test 4")
-})
+
+// app.post("/test", (req, res) => {
+//     res.send("Hello from post the data")
+// });
+
+// app.delete("/test",(req,res)=>{
+//     res.send("Hello from delete the data")
+// })
+
+
+
 const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
-    console.log("server is running on port 1000.....");
+    console.log("Server is running on port 1000.....");
 })
