518a261b0af33a398454a2ec798d4c12dcfe3a26
Jeevakumar-A <jeevajeeva40348@gmail.com>
2025-09-24 22:04:36 +0530

    Dynamic Data to signup by API

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ const express =require("express");//import express from "express";
 const app=express();//create an express application
 const UserModel =require("./models/user")
 
+app.use(express.json())
+//use()-->Handles the all routes-->so we put the express.json() inside it
+//express.json is the middleware function-->Hanles the incomming request with JSON payloads from the postman/react frontend
 app.post("/signup",async (req,res)=>{
+// console.log(req.body)//it from the postman api-->request to the body then the body data are JSON will send by api
 //Create a new instance of the user model
-const user =new UserModel({
-    FirstName:"jeeva",
-    LastName:"Kumar",
-    Email:"Jeevajeeva40348@gmail.com",
-    Password:"Jeevanaz"
-}
-)
+const user =new UserModel(req.body);
 await user.save();
 res.send("User data added successfully")
 })
