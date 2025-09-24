b320f29de3a8c5a7f29500fcd25979969806dc96
Jeevakumar-A <jeevajeeva40348@gmail.com>
2025-09-24 14:36:59 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
 const express =require("express");//import express from "express";
 const app=express();//create an express application
+const UserModel =require("./models/user")
 
-
-app.get("/test",(req,res,next)=>{
-
-console.log("Hello from the middleware 1");
-    next();
-},
-(req,res,next)=>{
-    console.log("Hello from the middleware 2");
-    next();
-},
-(req,res,next)=>{
-    console.log("Hello from the middleware 3");
-    next();
-    
-},
-(req,res)=>{
-    res.send("Hello from the Middleware 4");
+app.post("/signup",async (req,res)=>{
+//Create a new instance of the user model
+const user =new UserModel({
+    FirstName:"jeeva",
+    LastName:"Kumar",
+    Email:"Jeevajeeva40348@gmail.com",
+    Password:"Jeevanaz"
 }
 )
-
-// app.post("/test", (req, res) => {
-//     res.send("Hello from post the data")
-// });
-
-// app.delete("/test",(req,res)=>{
-//     res.send("Hello from delete the data")
-// })
+await user.save();
+res.send("User data added successfully")
+})
 
 
-
-const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
+ConnectDB().then(()=>{
+    console.log("Database connected successfully");
+    const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
     console.log("Server is running on port 1000.....");
+})
+}).catch((err)=>{
+    console.log("Database connection failed",err);
 })
