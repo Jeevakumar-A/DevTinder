808b3663de5bf934c75571971a12f8cd0963017d
Jeevakumar-A <jeevajeeva40348@gmail.com>
2025-09-24 14:30:24 +0530

    const express =require("express");//import express from "express"; const app=express();//create an express application
    app.get("/test",(req,res,next)=>{
    
    console.log("Hello from the middleware 1");
        next();
    },
    (req,res,next)=>{
        console.log("Hello from the middleware 2");
        next();
    },
    (req,res,next)=>{
        console.log("Hello from the middleware 3");
        next();
    },
    (req,res)=>{
        res.send("Hello from the Middleware 4");
    }
    )
    
    // app.post("/test", (req, res) => {
    //     res.send("Hello from post the data")
    // });
    
    // app.delete("/test",(req,res)=>{
    //     res.send("Hello from delete the data")
    // })
    
    const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
        console.log("Server is running on port 1000.....");
    })

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
+const {ConnectDB }=require("./config/database");//import mongoose from "./config/database.js"
 const express =require("express");//import express from "express";
 const app=express();//create an express application
+const UserModel =require("./models/user")
 
-
-app.get("/test",(req,res)=>{
-    res.send("Hello form get the data")
+app.post("/signup",async (req,res)=>{
+//Create a new instance of the user model
+const user =new UserModel({
+    FirstName:"jeeva",
+    LastName:"Kumar",
+    Email:"Jeevajeeva40348@gmail.com",
+    Password:"Jeevanaz"
+}
+)
+await user.save();
+res.send("User data added successfully")
 })
 
-// app.post("/test", (req, res) => {
-//     res.send("Hello from post the data")
-// });
 
-// app.delete("/test",(req,res)=>{
-//     res.send("Hello from delete the data")
-// })
-
-
-
-const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
+ConnectDB().then(()=>{
+    console.log("Database connected successfully");
+    const server =app.listen(1000,()=>{//start the server on port 1000-->Listen the request
     console.log("Server is running on port 1000.....");
 })
+}).catch((err)=>{
+    console.log("Database connection failed",err);
+})
+
